67a1a101c9ff9e9023ee416441fd79ff485184e2
cexes <mottacesar001@gmail.com>
2021-09-29 16:27:17 -0300

    uri

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+            
+            Scanner input = new Scanner (System.in);
+             
+            int A = input.nextInt();
+            int  B = input.nextInt();
+            int soma = A + B;
+             
+        
+             System.out.println("X = " + soma);
+            
+            
+    
+
+
+
+    }
+}
